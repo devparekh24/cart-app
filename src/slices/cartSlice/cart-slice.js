d7198d2606112c0e4e00fe5bd80df2cc88dda8a8
devparekh24 <devparekh2424@gmail.com>
2023-12-19 11:43:06 +0530

    created cart-slice & added dummy data

--- src/slices/cartSlice/cart-slice.js
+++ src/slices/cartSlice/cart-slice.js
@@ -5,15 +5,36 @@ const cartSlice = createSlice({
     initialState: {
         items: [],
         totalQuantity: 0,
-        totalAmount: 0
     },
     reducers: {
-        addItem(state, action) { 
+        addItem(state, action) {
             const item = action.payload
+            const existingItem = state.items.find(currItem => currItem.id === item.id)
+            if (!existingItem) {
+                state.items.push({
+                    itemId: item.id,
+                    price: item.price,
+                    quantity: 1,
+                    totalPrice: item.price,
+                    name: item.name
+                })
+            } else {
+                existingItem.quantity++;
+                existingItem.totalPrice = existingItem.totalPrice + item.price
+            }
         },
-        removeItem(state, action) { }
+        removeItem(state, action) {
+            const id = action.payload
+            const existingItem = state.items.find(item => item.id === id)
+            if (existingItem.quantity === 1) {
+                state.items = state.items.filter(item => item.id !== id)
+            } else {
+                existingItem.quantity--;
+                existingItem.totalPrice = existingItem.totalPrice - existingItem.price
+            }
+        }
     }
 })
 
-export default cartSlice.actions
-export const { addItem, removeItem } = cartSlice.reducer
+export const cartActions = cartSlice.actions
+export default cartSlice
--- src/store/index.js
+++ src/store/index.js
@@ -1,9 +1,11 @@
 import { configureStore } from '@reduxjs/toolkit'
 import uiSlice from '../slices/uiSlice/ui-slice'
+import cartSlice from '../slices/cartSlice/cart-slice'
 
 const store = configureStore({
     reducer: {
-        ui: uiSlice.reducer
+        ui: uiSlice.reducer,
+        cart: cartSlice.reducer
     }
 })
 
