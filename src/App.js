1dd9b6703f087e23d22cd24c87aa775999caf7dc
devparekh24 <devparekh2424@gmail.com>
2023-12-20 13:28:22 +0530

    added notification handler

--- src/App.js
+++ src/App.js
@@ -1,16 +1,29 @@
-import { useEffect } from 'react';
+import { Fragment, useEffect } from 'react';
 import Cart from './components/Cart/Cart';
 import Layout from './components/Layout/Layout';
 import Products from './components/Shop/Products';
-import { useSelector } from 'react-redux'
+import { useSelector, useDispatch } from 'react-redux'
+import { uiActions } from './slices/uiSlice/ui-slice';
+import Notification from './components/UI/Notification/Notification'
+
+let isInitial = true
 
 function App() {
+
   const isToggle = useSelector(state => state.ui.isToggle)
+  const isNotification = useSelector(state => state.ui.notification)
   const cart = useSelector(state => state.cart)
+  const dispatch = useDispatch()
 
   useEffect(() => {
 
     const sentCartData = async () => {
+
+      dispatch(uiActions.showNotification({
+        status: 'Pending',
+        title: 'Sending...',
+        message: 'Sending cart data!'
+      }))
 
       const res = await fetch('https://redux-cart-app-25cba-default-rtdb.asia-southeast1.firebasedatabase.app/cart.json', {
         method: 'PUT',
@@ -21,18 +34,40 @@ function App() {
         throw new Error('Sending cart data failed!')
       }
 
-      resData = await res.json()
+      dispatch(uiActions.showNotification({
+        status: 'success',
+        title: 'Success!',
+        message: 'Item added Successfully...!'
+      }))
     }
 
-  }, [cart])
+    if (isInitial) {
+      isInitial = false
+      return
+    }
+
+    sentCartData().catch((err) => {
+      dispatch(uiActions.showNotification({
+        status: 'error',
+        title: 'Error!',
+        message: 'Something went wrong!'
+      }))
+    })
+
+  }, [cart, dispatch])
 
   return (
-    <Layout>
-      {isToggle &&
-        <Cart />
+    <Fragment>
+      {
+        isNotification && <Notification status={isNotification.status} title={isNotification.title} message={isNotification.message} />
       }
-      <Products />
-    </Layout>
+      <Layout>
+        {isToggle &&
+          <Cart />
+        }
+        <Products />
+      </Layout>
+    </Fragment>
   );
 }
 
